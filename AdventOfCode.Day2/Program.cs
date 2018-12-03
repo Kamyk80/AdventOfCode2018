4d67b795be2335cfe60f948ea8723ed1299f2c2d
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2018-12-03 15:51:49 +0100

    Day 2 Part 2

--- AdventOfCode.Day2/Program.cs
+++ AdventOfCode.Day2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace AdventOfCode.Day2
 {
@@ -28,9 +29,41 @@ namespace AdventOfCode.Day2
             return twos * threes;
         }
 
+        private static string Prototype()
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            return lines
+                .SelectMany(l => lines, Compare)
+                .Where(c => c.diff == 1)
+                .Select(c => c.common)
+                .First();
+        }
+
+        private static (int diff, string common) Compare(string line1, string line2)
+        {
+            var common = new StringBuilder();
+            var diff = 0;
+
+            for (var i = 0; i < new[] {line1.Length, line2.Length}.Min(); i++)
+            {
+                if (line1[i] == line2[i])
+                {
+                    common.Append(line1[i]);
+                }
+                else
+                {
+                    diff++;
+                }
+            }
+
+            return (diff, common.ToString());
+        }
+
         private static void Main()
         {
             Console.WriteLine(Checksum());
+            Console.WriteLine(Prototype());
             Console.ReadKey(true);
         }
     }
