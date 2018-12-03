6e7320173bc51879cb9c094dd015100373d78a67
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2018-12-03 20:24:25 +0100

    Day 3 Part 2

--- AdventOfCode.Day3/Program.cs
+++ AdventOfCode.Day3/Program.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode.Day3
 
     internal static class Program
     {
-        private static int Overlap()
+        private static int ClaimsOverlap()
         {
             var regex = new Regex(@"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$");
             var claims = File.ReadAllLines("input.txt")
@@ -54,9 +54,40 @@ namespace AdventOfCode.Day3
             return result;
         }
 
+        private static int ClaimIntact()
+        {
+            var regex = new Regex(@"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$");
+            var claims = File.ReadAllLines("input.txt")
+                .Select(l => regex.Match(l))
+                .Select(m => new Claim
+                {
+                    Id = int.Parse(m.Groups[1].Captures[0].Value),
+                    Left = int.Parse(m.Groups[2].Captures[0].Value),
+                    Top = int.Parse(m.Groups[3].Captures[0].Value),
+                    Width = int.Parse(m.Groups[4].Captures[0].Value),
+                    Height = int.Parse(m.Groups[5].Captures[0].Value)
+                })
+                .ToList();
+
+            return claims
+                .First(c1 => claims
+                    .Where(c2 => c1 != c2)
+                    .All(c2 => !AreOverlapping(c1, c2)))
+                .Id;
+        }
+
+        private static bool AreOverlapping(Claim c1, Claim c2)
+        {
+            return c1.Left < c2.Left + c2.Width &&
+                   c1.Left + c1.Width > c2.Left &&
+                   c1.Top < c2.Top + c2.Height &&
+                   c1.Height + c1.Top > c2.Top;
+        }
+
         private static void Main()
         {
-            Console.WriteLine(Overlap());
+            Console.WriteLine(ClaimsOverlap());
+            Console.WriteLine(ClaimIntact());
             Console.ReadKey(true);
         }
     }
