8078d19a03b36bc5f18ac921d03d2060ab5abb06
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2018-12-05 17:02:58 +0100

    Day 5 Part 1

---
+++ AdventOfCode.Day5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Day5
+{
+    internal static class Program
+    {
+        private static int ReactedPolymer()
+        {
+            var polymer = new LinkedList<char>(File.ReadAllText("input.txt").Trim());
+            var current = polymer.First;
+
+            while (current.Next != null)
+            {
+                if (Math.Abs(current.Value - current.Next.Value) == 32)
+                {
+                    var next = current.Previous;
+                    polymer.Remove(current.Next);
+                    polymer.Remove(current);
+                    current = next ?? polymer.First;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+
+            return polymer.Count;
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine(ReactedPolymer());
+            Console.ReadKey(true);
+        }
+    }
+}
