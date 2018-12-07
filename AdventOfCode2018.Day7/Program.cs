8a4c212e40c42e0844516d631c67a8180162c815
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2018-12-07 18:38:49 +0100

    Day 7 Part 2

--- AdventOfCode2018.Day7/Program.cs
+++ AdventOfCode2018.Day7/Program.cs
@@ -8,7 +8,18 @@ namespace AdventOfCode2018.Day7
 {
     internal static class Program
     {
-        private static void Main()
+        public class Step
+        {
+            public bool Started { get; set; }
+            public bool Finished { get; set; }
+        }
+        public class Worker
+        {
+            public string Task { get; set; }
+            public int Time { get; set; }
+        }
+
+        private static string StepsOrder()
         {
             var regex = new Regex(@"^Step (?<before>\w) must be finished before step (?<after>\w) can begin.$");
             var rules = File.ReadAllLines("input.txt")
@@ -27,7 +38,7 @@ namespace AdventOfCode2018.Day7
                 .ToDictionary(s => s, s => false);
             var order = new StringBuilder();
 
-            while (!steps.All(s => s.Value))
+            while (steps.Any(s => !s.Value))
             {
                 var ready = steps
                     .Where(s => !s.Value)
@@ -42,7 +53,80 @@ namespace AdventOfCode2018.Day7
                 steps[ready] = true;
             }
 
-            Console.WriteLine(order);
+            return order.ToString();
+        }
+
+        private static int StepsTime()
+        {
+            var regex = new Regex(@"^Step (?<before>\w) must be finished before step (?<after>\w) can begin.$");
+            var rules = File.ReadAllLines("input.txt")
+                .Select(l => regex.Match(l))
+                .Select(m => new
+                {
+                    Before = m.Groups["before"].Value,
+                    After = m.Groups["after"].Value
+                })
+                .ToList();
+            var steps = rules
+                .Select(r => r.Before)
+                .Concat(rules
+                    .Select(r => r.After))
+                .Distinct()
+                .ToDictionary(s => s, s => new Step
+                {
+                    Started = false,
+                    Finished = false
+                });
+            var workers = Enumerable.Range(0, 5)
+                .Select(i => new Worker())
+                .ToList();
+            var time = -1;
+
+            while (steps.Any(s => !s.Value.Finished))
+            {
+                foreach (var worker in workers)
+                {
+                    if (worker.Time == 0 && worker.Task != null)
+                    {
+                        steps[worker.Task].Finished = true;
+                        worker.Task = null;
+                    }
+                }
+
+                foreach (var worker in workers)
+                {
+                    if (worker.Time == 0)
+                    {
+                        var ready = steps
+                            .Where(s => !s.Value.Started)
+                            .Where(s => rules
+                                .Where(r => r.After == s.Key)
+                                .All(r => steps[r.Before].Finished))
+                            .Select(s => s.Key)
+                            .OrderBy(s => s)
+                            .FirstOrDefault();
+
+                        if (ready != null)
+                        {
+                            worker.Task = ready;
+                            worker.Time = ready[0] - 4;
+                            steps[worker.Task].Started = true;
+                        }
+                    }
+
+                    worker.Time = worker.Time > 0 ? worker.Time - 1 : worker.Time;
+                }
+
+                time++;
+            }
+
+            return time;
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine(StepsOrder());
+            Console.WriteLine(StepsTime());
             Console.ReadKey(true);
         }
     }
