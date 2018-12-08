37fd2b37b601721e5d0b36cfac46aa03f46b7365
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2018-12-08 13:41:26 +0100

    Day 8 Part 2

--- AdventOfCode2018.Day8/Program.cs
+++ AdventOfCode2018.Day8/Program.cs
@@ -13,28 +13,55 @@ namespace AdventOfCode2018.Day8
             return seq.Current;
         }
 
-        private static int SumMetadata(IEnumerator<int> seq)
+        private static int MetadataSum(IEnumerator<int> seq)
         {
-            var nodes = seq.Read();
-            var values = seq.Read();
+            var nodesCount = seq.Read();
+            var metadataCount = seq.Read();
 
             return
-                Enumerable.Range(0, nodes)
-                    .Sum(n => SumMetadata(seq))
+                Enumerable.Range(0, nodesCount)
+                    .Sum(n => MetadataSum(seq))
                 +
-                Enumerable.Range(0, values)
-                    .Sum(v => seq.Read());
+                Enumerable.Range(0, metadataCount)
+                    .Sum(m => seq.Read());
+        }
+
+        private static int NodeValue(IEnumerator<int> seq)
+        {
+            var nodesCount = seq.Read();
+            var metadataCount = seq.Read();
+
+            var nodes = Enumerable.Range(0, nodesCount)
+                .Select(n => NodeValue(seq))
+                .ToList();
+
+            var metadata = Enumerable.Range(0, metadataCount)
+                .Select(m => seq.Read())
+                .ToList();
+
+            return nodesCount == 0
+                ? metadata
+                    .Sum(m => m)
+                : metadata
+                    .Where(m => m > 0 && m <= nodesCount)
+                    .Sum(m => nodes[m - 1]);
         }
 
         private static void Main()
         {
             var input = File.ReadAllText("input.txt")
                 .Split(" ")
-                .Select(int.Parse);
+                .Select(int.Parse)
+                .ToList();
 
             using (var seq = input.GetEnumerator())
             {
-                Console.WriteLine(SumMetadata(seq));
+                Console.WriteLine(MetadataSum(seq));
+            }
+
+            using (var seq = input.GetEnumerator())
+            {
+                Console.WriteLine(NodeValue(seq));
             }
 
             Console.ReadKey(true);
