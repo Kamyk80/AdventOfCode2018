a3abe50e9fd9c7e66ff6bf90a17672dc014cf0a4
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2018-12-08 13:15:40 +0100

    Day 8 Part 1

---
+++ AdventOfCode2018.Day8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018.Day8
+{
+    internal static class Program
+    {
+        private static int Read(this IEnumerator<int> seq)
+        {
+            seq.MoveNext();
+            return seq.Current;
+        }
+
+        private static int SumMetadata(IEnumerator<int> seq)
+        {
+            var nodes = seq.Read();
+            var values = seq.Read();
+
+            return
+                Enumerable.Range(0, nodes)
+                    .Sum(n => SumMetadata(seq))
+                +
+                Enumerable.Range(0, values)
+                    .Sum(v => seq.Read());
+        }
+
+        private static void Main()
+        {
+            var input = File.ReadAllText("input.txt")
+                .Split(" ")
+                .Select(int.Parse);
+
+            using (var seq = input.GetEnumerator())
+            {
+                Console.WriteLine(SumMetadata(seq));
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
